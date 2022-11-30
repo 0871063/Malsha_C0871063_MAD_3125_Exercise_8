3d63f7abf1b97eeb72f4d94798b899db8373c576
0871063 <c0871063@mylambton.ca>
2022-11-29 20:32:46 -0500

    ListView and Timer Object

--- app/src/main/java/com/example/exercise_8/MainActivity.java
+++ app/src/main/java/com/example/exercise_8/MainActivity.java
@@ -3,12 +3,79 @@ package com.example.exercise_8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button startBtn;
+    private Button lapBtn;
+    private TextView timerTV;
+    private ListView timerListView;
+    private ArrayList<Timer> timerList = new ArrayList<>();
+    private  boolean timerStarted ;
+    private Handler handler = new Handler();
+    private Runnable runnable;
+    private int delay = 1000;
+    private int lapCount = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBtn = findViewById(R.id.startBtn);
+        lapBtn = findViewById(R.id.lapBtn);
+        timerTV = findViewById(R.id.timerTV);
+        timerListView = findViewById(R.id.timerListView);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (timerStarted){
+                    startBtn.setText(getString(R.string.start));
+                    handler.removeCallbacks(runnable);
+                    lapBtn.setVisibility(View.GONE);
+                    timerTV.setText(getString(R.string.timer));
+                }else {
+                    startBtn.setText(getString(R.string.stop));
+                    timerStarted = true;
+                    lapBtn.setVisibility(View.VISIBLE);
+                    startTimer();
+                }
+            }
+        });
+        lapBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                lapCount += 1;
+                String time = timerTV.getText().toString();
+                String lap = "Lap " + String.valueOf(lapCount);
+                Timer timer = new Timer(lap ,time);
+                timerList.add(timer);
+
+            }
+        });
+
+        ArrayAdapter<Timer> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,timerList);
+        timerListView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+    }
+
+    private void startTimer() {
+        handler.postDelayed(runnable = new Runnable() {
+            public void run() {
+                handler.postDelayed(runnable, delay);
+                timerAction();
+            }
+        }, delay);
+    }
+
+    private void timerAction(){
+
     }
 }
---
+++ app/src/main/java/com/example/exercise_8/Timer.java
@@ -0,0 +1,27 @@
+package com.example.exercise_8;
+
+public class Timer {
+    private String lapCount;
+    private String time;
+
+    public String getLapCount() {
+        return lapCount;
+    }
+
+    public void setLapCount(String lapCount) {
+        this.lapCount = lapCount;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Timer(String lapCount, String time) {
+        this.lapCount = lapCount;
+        this.time = time;
+    }
+}
