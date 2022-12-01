e1339a1051368d6577e03f2dce268baf7e72c285
0871063 <c0871063@mylambton.ca>
2022-11-30 19:17:53 -0500

    Start Timer

--- app/src/main/java/com/example/exercise_8/MainActivity.java
+++ app/src/main/java/com/example/exercise_8/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private Runnable runnable;
     private int delay = 1000;
     private int lapCount = 0;
+    private int seconds = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
         lapBtn = findViewById(R.id.lapBtn);
         timerTV = findViewById(R.id.timerTV);
         timerListView = findViewById(R.id.timerListView);
-
+        lapBtn.setVisibility(View.GONE);
+        ArrayAdapter<Timer> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,timerList);
+        timerListView.setAdapter(adapter);
         startBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (timerStarted){
@@ -41,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
                     handler.removeCallbacks(runnable);
                     lapBtn.setVisibility(View.GONE);
                     timerTV.setText(getString(R.string.timer));
+                    timerStarted = false;
+                    seconds = 0;
+                    handler.removeCallbacks(runnable);
                 }else {
                     startBtn.setText(getString(R.string.stop));
                     timerStarted = true;
@@ -56,26 +63,44 @@ public class MainActivity extends AppCompatActivity {
                 String lap = "Lap " + String.valueOf(lapCount);
                 Timer timer = new Timer(lap ,time);
                 timerList.add(timer);
-
+                adapter.notifyDataSetChanged();
             }
         });
-
-        ArrayAdapter<Timer> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,timerList);
-        timerListView.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
-
     }
 
     private void startTimer() {
-        handler.postDelayed(runnable = new Runnable() {
-            public void run() {
-                handler.postDelayed(runnable, delay);
-                timerAction();
+        handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+
+            public void run()
+            {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                // Format the seconds into hours, minutes,
+                // and seconds.
+                String time
+                        = String
+                        .format(Locale.getDefault(),
+                                "%02d:%02d:%02d", hours,
+                                minutes, secs);
+
+                // Set the text view text.
+                timerTV.setText(time);
+
+                // If running is true, increment the
+                // seconds variable.
+                if (timerStarted) {
+                    seconds++;
+                }
+
+                // Post the code again
+                // with a delay of 1 second.
+                handler.postDelayed(this, 1000);
             }
-        }, delay);
+        });
     }
 
-    private void timerAction(){
-
-    }
 }
